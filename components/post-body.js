efdc02358623d9f98f01c9626b51626fec851ac1
michaelDpierce <michael@digiquatics.com>
2020-06-14 09:46:35 -0400

    Add lazy loading to Prismic HTML Serializer

--- components/post-body.js
+++ components/post-body.js
@@ -1,8 +1,26 @@
 
-import { RichText } from 'prismic-reactjs'
+import { RichText, Elements } from 'prismic-reactjs';
+
+const propsWithUniqueKey = function (props, key) {
+  return Object.assign(props || {}, { key });
+};
+
+const htmlSerializer = function(type, element, content, children, key) {
+  let props = {};
+
+  switch(type) {
+    case Elements.image:
+      props = { src: element.url , alt: element.alt || '', loading:"lazy" };
+      return React.createElement('img', propsWithUniqueKey(props, key));
+
+    // Return null to stick with the default behavior
+    default:
+      return null;
+  }
+};
 
 export default function PostBody({ content }) {
   return (
-    <RichText render={content} />
+    <RichText render={content} htmlSerializer={htmlSerializer} />
   )
 }
