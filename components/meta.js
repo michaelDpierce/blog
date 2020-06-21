c3cc28e746bbc737309aff6e472ed5da6015c50e
michaelDpierce <michael@digiquatics.com>
2020-06-21 12:40:23 -0400

    Meta data and favicons

--- components/meta.js
+++ components/meta.js
@@ -6,14 +6,40 @@ export default function Meta() {
       <meta charSet="utf-8" />
       <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
       <meta httpEquiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+      <meta name="robots" content="index,follow" />
+      <meta name="googlebot" content="index,follow" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta name="description" content="Michael Pierce's Blog" />
+      <meta content="#202B38" name="theme-color" />
+      <meta content="#202B38" name="msapplication-TileColor" />
+
+      {/* <meta content="/favicons/browserconfig.xml" name="msapplication-config" /> */}
+
+      <link href="/favicons/site.webmanifest" rel="manifest" />
       <link
         rel="shortcut icon"
         href="/favicon/favicon.ico"
         type="image/x-icon"
       />
       <link rel="icon" href="/favicon/favicon.ico" type="image/x-icon" />
+      <link
+        href="/favicons/apple-touch-icon.png"
+        rel="apple-touch-icon"
+        sizes="180x180"
+      />
+      <link
+        href="/favicons/favicon-32x32.png"
+        rel="icon"
+        sizes="32x32"
+        type="image/png"
+      />
+      <link
+        href="/favicons/favicon-16x16.png"
+        rel="icon"
+        sizes="16x16"
+        type="image/png"
+      />
+      {/* <link color="#202B38" href="/static/favicons/safari-pinned-tab.svg" rel="mask-icon" /> */}
     </Head>
   );
 }
