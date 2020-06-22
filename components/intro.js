86b661001af6b582a115f115279b446d218a325b
michaelDpierce <michael@digiquatics.com>
2020-06-22 10:16:48 -0400

    FIRE => F.I.R.E.

--- components/intro.js
+++ components/intro.js
@@ -79,7 +79,7 @@ export default function Intro() {
           rel="noopener"
           target="_blank"
         >
-          FIRE
+          F.I.R.E.
         </a>{' '}
         movement.
       </p>
