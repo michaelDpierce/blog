9790f6797b66768b7bc0ad2f7aa66b6eec10f67d
michaelDpierce <michael@digiquatics.com>
2020-06-22 08:19:12 -0400

    Next Js Serverless Target

--- next.config.js
+++ next.config.js
@@ -1,14 +1,11 @@
 require('dotenv').config()
 
 module.exports = {
+  target: 'serverless',
   env: {
-    HOST_URL:
-      process.env.HOST_URL || 'http://localhost:3000',
-    LANGUAGE_LOCALE:
-      process.env.LANGUAGE_LOCALE || 'en-us',
-    PRISMIC_API_TOKEN:
-      process.env.PRISMIC_API_TOKEN,
-    PRISMIC_REPOSITORY_NAME:
-      process.env.PRISMIC_REPOSITORY_NAME
+    HOST_URL: process.env.HOST_URL || 'http://localhost:3000',
+    LANGUAGE_LOCALE: process.env.LANGUAGE_LOCALE || 'en-us',
+    PRISMIC_API_TOKEN: process.env.PRISMIC_API_TOKEN,
+    PRISMIC_REPOSITORY_NAME: process.env.PRISMIC_REPOSITORY_NAME,
   },
-}
+};
