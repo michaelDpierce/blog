b0e68f362b8fe3cc166aaaf02209d185de419713
michaelDpierce <matchmike1313@gmail.com>
2021-02-24 15:12:55 -0500

    Fallback to blog for host

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ module.exports = {
   target: 'serverless',
   env: {
     FATHOM_SITE: process.env.FATHOM_SITE,
-    HOST_URL: process.env.HOST_URL || 'http://localhost:3000',
+    HOST_URL: process.env.HOST_URL || 'https://mpierce.blog',
     LANGUAGE_LOCALE: process.env.LANGUAGE_LOCALE || 'en-us',
     PRISMIC_API_TOKEN: process.env.PRISMIC_API_TOKEN,
     PRISMIC_REPOSITORY_NAME: process.env.PRISMIC_REPOSITORY_NAME,
